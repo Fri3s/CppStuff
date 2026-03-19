b1c6a74f76abffeb02be8c2660d263360202f98f
Fri3s <123522366+Fri3s@users.noreply.github.com>
2026-03-19 22:08:39 +0000

    optimize

--- USACO_Guide/simulation.cpp
+++ USACO_Guide/simulation.cpp
@@ -10,32 +10,22 @@ using namespace std;
 int main(){
 	freopen("shell.in", "r", stdin);
     freopen("shell.out", "w", stdout);
-	
+
     ios::sync_with_stdio(false);
 	cin.tie(nullptr);
 	cout.tie(nullptr);
 
 	int n;
 	cin >> n;
-	vector<int> guesses(n);
-	vector<vector<int>> positions(n);
-	vector<int> currentPos{1,2,3};
-	for (int i = 0; i < n; i++) {
+	vector<int> positions{0,1,2};
+	vector<int> scores(3,0);
+	for(int i = 0; i < n; i++){
 		int a, b, g;
 		cin >> a >> b >> g;
-		swap(currentPos[a-1], currentPos[b-1]);
-		positions[i] = currentPos;
-		guesses[i] = g;
-	}
-    int i = 3;
-	vector<int> scores(3, 0);
-	while(i){
-		for (int p = 0; p < n; p++){
-			if (positions[p][guesses[p] - 1] == i) scores[i-1]++;
-		}
-		i--;
+		a--, b--, g--;
+		scores[positions[g]]++;
+		swap(positions[a], positions[b]);
 	}
 
-	sort(scores.begin(), scores.end());
-	cout << scores[2] << "\n";
+	cout << max({scores[0], scores[1], scores[2]}) << "\n";
 }
