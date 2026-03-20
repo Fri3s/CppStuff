beeb16de826250c4ff11b4886c552cb8bd3b1b58
Fri3s <123522366+Fri3s@users.noreply.github.com>
2026-03-20 19:57:37 +0000

    mixingmilk

--- USACO_Guide/simulation.cpp
+++ USACO_Guide/simulation.cpp
@@ -4,9 +4,35 @@
 
 using namespace std;
 
+void pourBucket(vector<tuple<int, int, int>>& curretnAmounts, int i){
+
+}
+
+int main(){
+	freopen("mixmilk.in", "r", stdin);
+    freopen("mixmilk.out", "w", stdout);
+
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	vector<int> capacities(3);
+	vector<int> amounts(3);
+
+	for (int i = 0; i < 3; i++) cin >> capacities[i] >> amounts[i];
+
+	for (int i = 0; i < 100; i++){
+		int b = i%3, b2 = (i+1)%3;
+
+		int pourAmount = min(capacities[b2] - amounts[b2], amounts[b]);
+		amounts[b] -= pourAmount, amounts[b2] += pourAmount;
+	}
+
+	for (auto& a : amounts) cout << a << "\n";
+}
+
+/*
 //SHELL GAME 2019 DECEMBER
-
-
 int main(){
 	freopen("shell.in", "r", stdin);
     freopen("shell.out", "w", stdout);
@@ -29,3 +55,4 @@ int main(){
 
 	cout << max({scores[0], scores[1], scores[2]}) << "\n";
 }
+*/
